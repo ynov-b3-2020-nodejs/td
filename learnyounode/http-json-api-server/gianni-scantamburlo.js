2ff982ab75b3f3ae68d56e8e3d7ed927b029ea98
XDayonline <32893447+XDayonline@users.noreply.github.com>
2019-10-14 17:21:59 +0200

    Update gianni-scantamburlo.js

--- learnyounode/http-json-api-server/gianni-scantamburlo.js
+++ learnyounode/http-json-api-server/gianni-scantamburlo.js
@@ -1,7 +1,6 @@
 const port = process.argv[2];
-const path = '/api/parsetime';
 const http = require('http');
-let url = require('url');
+const url = require('url');
 
 function parseTime(time) {
     return {
@@ -23,20 +22,17 @@ let parseQuery = function (url) {
             return parseTime(new Date(url.query.iso));
         case '/api/unixtime':
             return unixTime(new Date(url.query.iso));
-        default:
-            return 'Rentrez une bonne url'
     }
 };
 
-http.createServer(function (request, response) {
+const server = http.createServer(function (request, response) {
     if (request.method === 'GET') {
         response.writeHead(200, {'Content-Type': 'application/json'});
-        url = url.parse(request.url, true);
-        response.end(JSON.stringify(parseQuery(url)))
+        const myUrl = url.parse(request.url, true);
+        response.end(JSON.stringify(parseQuery(myUrl)))
     } else {
         response.writeHead(405);
         response.end()
     }
-}).listen(+port, function () {
-    console.log('Serveur en écoute sur http://localhost:%s', port)
 });
+server.listen(port);

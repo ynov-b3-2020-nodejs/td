e7d8195197d69f216a64759fe45ca5df7677cc69
geoffreyparrier <47033480+geoffreyparrier@users.noreply.github.com>
2019-10-14 17:33:42 +0200

    Update geoffrey-parrier.js

--- learnyounode/http-json-api-server/geoffrey-parrier.js
+++ learnyounode/http-json-api-server/geoffrey-parrier.js
@@ -6,21 +6,32 @@ const port = process.argv[2];
 http.createServer((req, res) => {
 
     const parsedUrl = new URL(req.url, 'http://localhost/');
+
     const date = new Date(parsedUrl.searchParams.get('iso'));
 
     res.writeHead(200, {'Content-type': 'application/JSON'});
 
-    if (req.url.match('/api/parsetime')) {
-        res.write(JSON.stringify({
-            hour: date.getHours(),
-            minute: date.getMinutes(),
-            second: date.getSeconds()
-        }));
+    switch (parsedUrl.pathname) {
+        case '/api/parsetime':
+            res.write(JSON.stringify({
+                hour: date.getHours(),
+                minute: date.getMinutes(),
+                second: date.getSeconds()
+            }));
+            res.end();
+            break;
 
-    } else if (req.url.match('/api/unixtime')) {
-        res.write(JSON.stringify({
-            unixtime: date.getTime()
-        }));
+        case '/api/unixtime':
+            res.write(JSON.stringify({
+                unixtime: date.getTime()
+            }));
+            res.end();
+            break;
+
+        default:
+            res.writeHead(404, '404 page not found');
+            res.end();
+            break;
     }
-    res.end();
+
 }).listen(port);

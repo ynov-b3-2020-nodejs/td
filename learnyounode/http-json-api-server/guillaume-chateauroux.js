0de48602aae5be62ef719e3f9a1724b8694d50bd
ChxGuillaume <43764147+ChxGuillaume@users.noreply.github.com>
2019-10-14 17:03:51 +0200

    Update guillaume-chateauroux.js

--- learnyounode/http-json-api-server/guillaume-chateauroux.js
+++ learnyounode/http-json-api-server/guillaume-chateauroux.js
@@ -4,35 +4,38 @@ const [node, path, port] = process.argv;
 const server = http.createServer((req, res) => {
     const parsedUrl = new URL(req.url, 'http://localhost/');
     const iso = parsedUrl.searchParams.get('iso');
+    const date = new Date(iso);
 
     if (!iso) {
         res.writeHead(400);
         res.write('No ISO parameter specified.');
         res.end();
     } else {
-        const date = new Date(iso);
-
         if (!date) {
             res.writeHead(400);
             res.write('Bad date format.');
             res.end();
         } else {
-            if (parsedUrl.pathname === '/api/parsetime') {
-                res.writeHead(200, {'Content-Type': 'application/json'});
-                res.write(JSON.stringify({
-                    hour: date.getHours(),
-                    minute: date.getMinutes(),
-                    second: date.getSeconds(),
-                }));
-            } else if (parsedUrl.pathname === '/api/unixtime') {
-                res.writeHead(200, {'Content-Type': 'application/json'});
-                res.write(JSON.stringify({
-                    unixtime: date.getTime()
-                }));
-            } else {
-                res.writeHead(404);
-                res.write('Not found.');
+            switch (parsedUrl.pathname) {
+                case '/api/parsetime':
+                    res.writeHead(200, {'Content-Type': 'application/json'});
+                    res.write(JSON.stringify({
+                        hour: date.getHours(),
+                        minute: date.getMinutes(),
+                        second: date.getSeconds(),
+                    }));
+                    break;
+                case '/api/unixtime':
+                    res.writeHead(200, {'Content-Type': 'application/json'});
+                    res.write(JSON.stringify({
+                        unixtime: date.getTime()
+                    }));
+                    break;
+                default:
+                    res.writeHead(404);
+                    res.write('Not found.');
             }
+
             res.end();
         }
     }

5b2a5d92f2f26bfd64629109358a52c0ff98e59d
Nicolas Notararigo <nicolas.notararigo@ynov.com>
2019-10-14 17:00:59 +0200

    Create nicolas_notararigo.js

---
+++ learnyounode/http-json-api-server/nicolas_notararigo.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const port = process.argv[2];
+
+const server = http.createServer((req, res) => {
+    const parseUrl = new URL(req.url, 'http://localhost/');
+    const time = new Date(parseUrl.searchParams.get('iso'));
+    let result;
+
+    if (parseUrl.pathname === '/api/unixtime') {
+        result = {
+            "unixtime": time.getTime()
+        };
+    }else if (parseUrl.pathname === '/api/parsetime'){
+        result = {
+            "hour": time.getHours(),
+            "minute": time.getMinutes(),
+            "second": time.getSeconds()
+        };
+    }
+
+    if(result){
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(result));
+    }else{
+        res.writeHead(400, "Bad URL");
+        res.end();
+    }
+
+});
+
+server.listen(port);

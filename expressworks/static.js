1438087d0904d406f807151408762f6ce8e9650f
Mathieu TUDISCO <oss@mathieutu.dev>
2019-10-15 10:50:58 +0200

    Update static.js

--- expressworks/static.js
+++ expressworks/static.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const port = process.argv[2];
+const path = process.argv[3];
 
-app.use(express.static(process.argv[3]));
+app.use(express.static(path));
 
 app.listen(port);
